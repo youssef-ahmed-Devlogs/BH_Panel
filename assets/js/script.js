706a6514e00bf2d425907d9ffe842a4290e54d01
youssefahmeddevlogs <youssefahmedsup@gmail.com>
2022-02-08 06:46:53 +0200

    login page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,34 @@
 // ======== START SIDEBAR OPERATIONS ========
 
-const bhContent = document.querySelector(".bh__content");
-const hamburgBtn = document.querySelector(".bh__header-hamburg__btn");
+if (document.querySelector(".bh__header-hamburg__btn") != null) {
+  const bhContent = document.querySelector(".bh__content");
+  const hamburgBtn = document.querySelector(".bh__header-hamburg__btn");
 
-hamburgBtn.addEventListener("click", () => {
-  bhContent.classList.toggle("sidebar__close");
-});
+  hamburgBtn.addEventListener("click", () => {
+    bhContent.classList.toggle("sidebar__close");
+  });
 
-const sidebarLink = document.querySelectorAll(
-  ".bh_sidebar-links-list .sidebar__link"
-);
+  const sidebarLink = document.querySelectorAll(
+    ".bh_sidebar-links-list .sidebar__link"
+  );
 
-sidebarLink.forEach((link) => {
-  if (link.classList.contains("has__sub-list")) {
-    link.innerHTML += '<i class="fas fa-chevron-left"></i>';
+  sidebarLink.forEach((link) => {
+    if (link.classList.contains("has__sub-list")) {
+      link.innerHTML += '<i class="fas fa-chevron-left"></i>';
 
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
 
-      const subLinksList = link.parentElement.querySelector(".sub__links-list");
-      const chevronIcon = link.querySelector("i");
+        const subLinksList =
+          link.parentElement.querySelector(".sub__links-list");
+        const chevronIcon = link.querySelector("i");
 
-      subLinksList.classList.toggle("open");
-      chevronIcon.classList.toggle("fa-chevron-up");
-    });
-  }
-});
+        subLinksList.classList.toggle("open");
+        chevronIcon.classList.toggle("fa-chevron-up");
+      });
+    }
+  });
+}
 
 // ======== END SIDEBAR OPERATIONS ========
 
@@ -185,4 +188,147 @@ if (document.querySelector("#sales-chart") != null) {
   salesChart.render();
 }
 
+// user sales chart
+if (document.querySelector("#user__sales-chart") != null) {
+  var salesOptions = {
+    series: [
+      {
+        name: "الأول",
+        data: [100, 200, 300, 349, 53, 58, 500, 10, 66],
+        color: "#39B3F9",
+      },
+      {
+        name: "الثاني",
+        data: [300, 485, 101, 98, 30, 145, 91, 114, 94],
+        color: "#1686C6",
+      },
+    ],
+    chart: {
+      type: "bar",
+      height: 350,
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "60%",
+        endingShape: "rounded",
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      categories: [
+        "الجمعة",
+        "الخميس",
+        "الاربعاء",
+        "الثلاثاء",
+        "الأثنين",
+        "الأحد",
+        "السبت",
+      ],
+    },
+    fill: {
+      opacity: 1,
+      colors: ["#39B3F9", "#1686C6"],
+    },
+    tooltip: {
+      y: {
+        formatter: function (val) {
+          return `${val}ج.م`;
+        },
+      },
+    },
+  };
+
+  var salesChart = new ApexCharts(
+    document.querySelector("#user__sales-chart"),
+    salesOptions
+  );
+  salesChart.render();
+}
+
+// messages chart
+if (document.querySelector("#messages-chart") != null) {
+  var options = {
+    series: [
+      {
+        name: "الأولى",
+        data: [31, 100, 28, 290, 42, 109, 500, 60],
+        color: "#0A50F6",
+      },
+      {
+        name: "الثانية",
+        data: [11, 300, 45, 32, 400, 52, 41, 100],
+        color: "#84D2FF",
+      },
+    ],
+    chart: {
+      height: 350,
+      type: "area",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    xaxis: {
+      categories: [
+        "يناير",
+        "فبراير",
+        "مارس",
+        "ابريل",
+        "مايو",
+        "يونيو",
+        "يوليو",
+        "اغسطس",
+        "سبتمبر",
+        "اكتوبر",
+        "نوفمبر",
+        "ديسمبر",
+      ],
+    },
+    tooltip: {
+      x: {
+        format: "dd/MM/yy HH:mm",
+      },
+    },
+  };
+
+  var chart = new ApexCharts(
+    document.querySelector("#messages-chart"),
+    options
+  );
+  chart.render();
+}
+
 // ======== END COLUMN CHARTS ========
+
+// ======== START LOGIN FORM ========
+
+if (document.querySelector(".login__page form") != null) {
+  const loginForm = document.querySelector(".login__page form");
+  loginForm.addEventListener("submit", (e) => {
+    const username = loginForm.username;
+    const password = loginForm.password;
+
+    if (username.value == "" || password.value == "") {
+      e.preventDefault();
+      console.log("Empty");
+      showErorrMsg(username, "يجب إدخال اسم المستخدم");
+      showErorrMsg(password, "يجب إدخال كلمة المرور ");
+    }
+  });
+
+  function showErorrMsg(input, err) {
+    const errElement = input.parentElement.querySelector(".text-err");
+    if (input.value == "") {
+      errElement.innerHTML = err;
+    } else {
+      errElement.innerHTML = "";
+    }
+  }
+}
+
+// ======== END LOGIN FORM ========
